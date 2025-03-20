590130168dc045af21b6071ae4ec28f5011e2622
ab1273 <kokixchix@gmail.com>
2025-03-20 10:29:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Website loaded');
+});
+function redirectToPage() {
+    window.location.href = 'https://www.example.com';
+}
